631b63ec762b249e5ca5580ba9cfdbc2508ebd18
Felipe Almeida <felipedovale1997@gmail.com>
2020-08-28 11:46:03 -0300

    Adicionando swagger ao projeto

--- PesquisaAPI/Startup.cs
+++ PesquisaAPI/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using PesquisaAPI.DB;
 using PesquisaAPI.Repository;
 using PesquisaAPI.Repository.Interfaces;
@@ -31,34 +32,50 @@ namespace PesquisaAPI
             services.AddScoped<IRespostas, RespostasRepository>();
             services.AddScoped<ITiposRespostas, TiposRespostasRepository>();
             services.AddControllers().AddNewtonsoftJson();
-            services.AddMvc(setupAction => {
+            services.AddMvc(setupAction =>
+            {
                 setupAction.EnableEndpointRouting = false;
             }).AddJsonOptions(jsonOptions =>
             {
                 jsonOptions.JsonSerializerOptions.PropertyNamingPolicy = null;
             })
             .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            //Swagger
+            services.AddSwaggerGen(cfg =>
+            {
+                cfg.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Pesquisa de Satisfação",
+                });
+            });
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseDeveloperExceptionPage();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+
+        app.UseSwagger();
+        app.UseSwaggerUI(cfg =>
+        {
+            cfg.SwaggerEndpoint("/swagger/v1/swagger.json", "PesquisaSatisfacao");
+        });
     }
+}
 }
